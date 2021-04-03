a4ae35af562494560584b4e1eb2b83d69051bc01
Lándori Dávid <info@honlapvarazslo.com>
2021-04-03 12:34:20 +0200

    ✨   Add motor slugify

--- api/motor/models/motor.js
+++ api/motor/models/motor.js
@@ -2,18 +2,34 @@ const index = 'motor';
 
 module.exports = {
   lifecycles: {
-    async afterCreate(result) {
-      const searchObject = strapi.services.motor.getSearchObject(result);
-      strapi.services.algolia.saveObject(searchObject, index);
+    async beforeCreate(data) {
+      console.log(data);
+      data.slug = await strapi.services.motor.generateUniqueSlug(null, [
+        data.marka,
+        data.tipus,
+        data.kivitel,
+        data.gyartas_ev,
+      ]);
     },
-
+    async afterCreate(result) {
+      // const searchObject = strapi.services.motor.getSearchObject(result);
+      // strapi.services.algolia.saveObject(searchObject, index);
+    },
+    async beforeUpdate(params, data) {
+      data.slug = await strapi.services.motor.generateUniqueSlug(params.id, [
+        data.marka,
+        data.tipus,
+        data.kivitel,
+        data.gyartas_ev,
+      ]);
+    },
     async afterUpdate(result) {
-      const searchObject = strapi.services.motor.getSearchObject(result);
-      strapi.services.algolia.saveObject(searchObject, index);
+      // const searchObject = strapi.services.motor.getSearchObject(result);
+      // strapi.services.algolia.saveObject(searchObject, index);
     },
 
     async afterDelete(result) {
-      strapi.services.algolia.deleteObject(result.id, index);
+      // strapi.services.algolia.deleteObject(result.id, index);
     },
   },
 };
--- api/motor/services/motor.js
+++ api/motor/services/motor.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const slugify = require('slugify');
 
 module.exports = {
   getSearchObject(result) {
@@ -8,5 +9,30 @@ module.exports = {
       id: result.id,
       slug: result.slug,
     };
+  },
+  async generateUniqueSlug(id, slugArray) {
+    let slug;
+    let isUnique;
+
+    let i = 0;
+    do {
+      slug = slugify(slugArray.join('-').trim(), { lower: true, strict: true });
+      if (i > 0) {
+        slug = slug.concat(`-${i}`);
+      }
+
+      const motorssWithSameSlug = await strapi.services.motor.find(
+        {
+          ...(id ? { id_ne: id } : null),
+          slug,
+        },
+        []
+      );
+      isUnique = motorssWithSameSlug.length === 0;
+
+      i++;
+    } while (!isUnique);
+
+    return slug;
   },
 };
